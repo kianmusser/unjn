d8b05ae3bc28c8679f6d6b40e4a2f74ef4092c4a
Kian Musser <kian@kianmusser.com>
2024-02-17 13:00:23 +0000

    wip

--- server/server.go
+++ server/server.go
@@ -1,6 +1,16 @@
 package server
 
-import "github.com/mmcdole/gofeed"
+import (
+	"fmt"
+	"time"
+
+	"github.com/mmcdole/gofeed"
+)
+
+type Job struct {
+	Title, Description, Link string
+	Date                     time.Time
+}
 
 type Server struct {
 	ntfyTopic     string
@@ -8,11 +18,39 @@ type Server struct {
 	feedParser    *gofeed.Parser
 }
 
-func (s *Server) ParseRssFeed(rssUrl) {
-	s.feedParser.ParseURL()
+func (s *Server) ParseRssFeed(rssUrl string) []Job {
+	feed, err := s.feedParser.ParseURL(rssUrl)
+	if err != nil {
+		panic(err)
+	}
+	jobs := make([]Job, len(feed.Items))
+	for _, f := range feed.Items {
+		cur := Job{
+			Title:       f.Title,
+			Description: f.Description,
+			Link:        f.Link,
+			Date:        *f.PublishedParsed,
+		}
+		jobs = append(jobs, cur)
+	}
+	return jobs
+}
+
+func (s *Server) Notify(job Job) {
+	fmt.Printf("Title: %s\n", job.Title)
+}
+
+func (s *Server) Fetch() {
+	for _, url := range s.upworkRssUrls {
+		jobs := s.ParseRssFeed(url)
+		for _, job := range jobs {
+			s.Notify(job)
+		}
+	}
 }
 
 func (s *Server) Run() {
+	s.Fetch()
 }
 
 func NewServer(ntfyTopic string, upworkRssUrls []string) *Server {
