f217ec694f64b9a3e9272e1ad8ffaf91cac0d30f
Kian Musser <kian@kianmusser.com>
2024-02-17 13:28:54 +0000

    fix error with blank entries AND don't renotify on seen entries AND rerun every so often

--- server/server.go
+++ server/server.go
@@ -16,6 +16,7 @@ type Server struct {
 	ntfyTopic     string
 	upworkRssUrls []string
 	feedParser    *gofeed.Parser
+	seenJobs      map[string]bool
 }
 
 func (s *Server) ParseRssFeed(rssUrl string) []Job {
@@ -24,14 +25,14 @@ func (s *Server) ParseRssFeed(rssUrl string) []Job {
 		panic(err)
 	}
 	jobs := make([]Job, len(feed.Items))
-	for _, f := range feed.Items {
+	for i, f := range feed.Items {
 		cur := Job{
 			Title:       f.Title,
 			Description: f.Description,
 			Link:        f.Link,
 			Date:        *f.PublishedParsed,
 		}
-		jobs = append(jobs, cur)
+		jobs[i] = cur
 	}
 	return jobs
 }
@@ -41,18 +42,32 @@ func (s *Server) Notify(job Job) {
 }
 
 func (s *Server) Fetch() {
+	fmt.Println("fetching...")
 	for _, url := range s.upworkRssUrls {
 		jobs := s.ParseRssFeed(url)
 		for _, job := range jobs {
-			s.Notify(job)
+			if !s.seenJobs[job.Link] {
+				s.seenJobs[job.Link] = true
+				s.Notify(job)
+			}
 		}
 	}
 }
 
 func (s *Server) Run() {
+	ticker := time.NewTicker(5 * time.Second)
 	s.Fetch()
+	for {
+		<-ticker.C
+		s.Fetch()
+	}
 }
 
 func NewServer(ntfyTopic string, upworkRssUrls []string) *Server {
-	return &Server{ntfyTopic: ntfyTopic, upworkRssUrls: upworkRssUrls, feedParser: gofeed.NewParser()}
+	return &Server{
+		ntfyTopic:     ntfyTopic,
+		upworkRssUrls: upworkRssUrls,
+		feedParser:    gofeed.NewParser(),
+		seenJobs:      make(map[string]bool),
+	}
 }
